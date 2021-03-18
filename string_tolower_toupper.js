a0647562b44c2c6b735b114ab1c641c19ed2b3be
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-03-18 08:03:14 -0500

    Update string_tolower_toupper.js
    JavaScript function that takes a string which has lower and upper case letters as a parameter and converts upper case letters to lower case, and lower case letters to upper case

--- string_tolower_toupper.js
+++ string_tolower_toupper.js
@@ -2,3 +2,18 @@ var test = "tu veux faire un test";
 var res = test.toUpperCase();//test.toLowerCase();  Methode de l'objet String pour transformer en miniscule , majuscule
 console.log(res);
 alert(res);
+//
+
+function swapcase(s){
+    var res;
+    for(var i = 0 ; i < s.length; i++){
+        let min = s[i].toLowerCase();
+        let max = s[i].toUpperCase();
+        if(s[i] === min)
+           res = res + max;
+        if(s[i] === max)
+           res = res + min;
+    }
+    return res;
+}
+swapcase("motsebo zossie patrick");
