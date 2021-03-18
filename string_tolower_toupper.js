40ef3a62b45f251fd5d2832655017c00b7ef0c5d
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-03-18 06:35:32 -0500

    Create string_tolower_toupper.js

---
+++ string_tolower_toupper.js
@@ -0,0 +1,4 @@
+var test = "tu veux faire un test";
+var res = test.toUpperCase();//test.toLowerCase();  Methode de l'objet String pour transformer en miniscule , majuscule
+console.log(res);
+alert(res);
